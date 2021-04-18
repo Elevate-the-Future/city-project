0c1ce116e760c9c7f36379908d276da3c4c6522b
SecretlyGod <SecretlyGod@users.noreply.github.com>
2021-04-17 21:29:40 -0400

    4 17

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Yourtest Vue.js App"/>
   </div>
 </template>
 
@@ -17,6 +17,7 @@ export default {
 </script>
 
 <style>
+@import('./assets/tailwind.css')
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
